783966f2a8a7ada21fda0a7b3664c7ca23e4c85e
unknown <imdamann690@aol.com>
2025-12-31 11:36:32 -0500

    Fix sakhal config check

--- check_sakhal_config.js
+++ check_sakhal_config.js
@@ -6,7 +6,7 @@ async function checkSakhalConfig() {
         
         try {
             const result = await client.query(
-                'SELECT guild_id, nitrado_service_id, map_name, killfeed_channel_id, connections_channel_id, admin_channel_id, restart_hours FROM guild_configs WHERE guild_id = $1',
+                'SELECT * FROM guild_configs WHERE guild_id = $1',
                 ['1445957198000820316']
             );
             
@@ -15,14 +15,7 @@ async function checkSakhalConfig() {
             } else {
                 const config = result.rows[0];
                 console.log('\n=== SAKHAL SERVER CONFIG ===');
-                console.log('Guild ID:', config.guild_id);
-                console.log('Nitrado Service ID:', config.nitrado_service_id);
-                console.log('Map:', config.map_name);
-                console.log('Restart Hours:', config.restart_hours);
-                console.log('\nChannels:');
-                console.log('  Killfeed Channel:', config.killfeed_channel_id);
-                console.log('  Connections Channel:', config.connections_channel_id);
-                console.log('  Admin Channel:', config.admin_channel_id);
+                console.log(JSON.stringify(config, null, 2));
             }
             
         } finally {
